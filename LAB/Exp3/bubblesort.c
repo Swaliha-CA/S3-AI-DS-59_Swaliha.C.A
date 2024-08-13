2878fa564e519cf1b39188c821a8d7cfd987be08
Swaliha-CA <swalihaswali100@gmail.com>
2024-08-13 12:25:04 +0530

    Create bubblesort.c

---
+++ LAB/Exp3/bubblesort.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+//program to seach an element
+//swaliha_59
+
+   void main()
+   {
+    int search,n,i,arr[10];
+    printf("enter the number of elements");
+    scanf("%d",&n);
+    printf("enter the elements");
+     for(i=0;i<n;i++)
+		 {
+		   scanf("%d",&arr[i]);
+		   }
+       printf("enter the search element");
+       scanf("%d",&search);
+       for(i=0;i<n;i++)
+       {
+		   
+		    
+				if(arr[i]==search)
+				{
+				printf("%d is present at location %d",search,i+1);
+				break;
+		    	}
+		    }
+		    
+    
+        
+       
+         {
+           
+        if(i==n)
+        {
+        
+         printf("%d is not  present in array",search);
+         }
+         }
+         }
+         
